84115233a7bcf6ca4dc54f2cf7475b5d442590f0
phuclamqwe-dev <phuclamqwe@gmail.com>
2026-04-06 20:03:43 +0700

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,9 @@ const cors = require("cors");
 const fs = require("fs");
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: "*"
+}));
 app.use(express.json());
 
 const FILE = "data.json";
